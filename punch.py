897c4adbdd6ff2a9587ad31d4e0e1a4a4386800d
Kim Salmi <kim.salmi@sanoma.com>
2018-02-23 14:02:49 +0200

    add debug option, add feature to see balance

--- punch.py
+++ punch.py
@@ -3,9 +3,11 @@ parser = argparse.ArgumentParser()
 parser.add_argument("direction", help="in or out")
 parser.add_argument("--time", help="Punch specific time eg. 10:15")
 parser.add_argument("--date", help="Punch specific date eg. 01.01.2018")
+parser.add_argument("--debug", help="Do not really punch", action="store_true", default=False)
 args = parser.parse_args()
 
-r = regweb.regweb('username', 'password', 'http://domain')
+r = regweb.regweb('username', 'password', 'http://domain', debug=args.debug)
+
 r.getCookie()
 r.login()
 if args.time and args.date:
--- regweb.py
+++ regweb.py
@@ -2,18 +2,21 @@ import requests, datetime
 from bs4 import BeautifulSoup
 
 class regweb():
-    def __init__(self, username, password, server):
-        self.punchUrl = server+"/action-punch-add-worktime.asp?cardnum=%s&pdate=%s&seluser=%s&ptime=%s&pdir=%s&pcom=0&sb-fix-save=Tallenna"
+    def __init__(self, username, password, server, debug=False):
+        self.punchUrl = server + "/action-punch-add-worktime.asp?cardnum=%s&pdate=%s&seluser=%s&ptime=%s&pdir=%s&pcom=0&sb-fix-save=Tallenna"
+        self.debugPunchUrl = server + "/fixpunch.asp?curdate=%s&ptype=0"
         self.frontUrl = server+"/login.asp"
         self.loginUrl = server+"/action-login.asp"
         self.username = username
         self.password = password
         self.url = ""
+        self.debug = debug
 
         self.cardnum = ""
         self.seluser = ""
         self.pdate = ""
         self.ptime = ""
+        self.balance = ""
 
         self.direction = {
             'out': 0,
@@ -38,15 +41,19 @@ class regweb():
         r = requests.post(self.url, data=data, cookies=self.cookies)
         if r.status_code == 200:
             soup = BeautifulSoup(r.text, 'html.parser')
-            soup = soup.find_all("div", "user")[0].find_all("p")[0]
-            self.seluser = soup.find_all("span")[1].string.strip()
-            self.cardnum = soup.find_all("span")[2].string.strip()
+            soup_user = soup.find_all("div", "user")[0].find_all("p")[0]
+            self.seluser = soup_user.find_all("span")[1].string.strip()
+            self.cardnum = soup_user.find_all("span")[2].string.strip()
+            self.balance = soup.find_all("div", "tbpp-balances")[0].find_all("td")[1].string.strip()
         else:
             print("Could not login")
             exit(0)
 
     def generatePunchUrl(self):
-        self.url = self.punchUrl % (self.cardnum, self.pdate, self.seluser, self.ptime, self.pdir)
+        if self.debug:
+            self.url = self.debugPunchUrl % (self.pdate)
+        else:
+            self.url = self.punchUrl % (self.cardnum, self.pdate, self.seluser, self.ptime, self.pdir)
        
     def punch(self, direction, time=False, date=False):
         self.pdir = self.direction[direction]
@@ -60,9 +67,11 @@ class regweb():
             self.pdate = datetime.datetime.now().strftime("%-d.%m.%Y")
 
         self.generatePunchUrl()
-        # print(self.url)
+
         r = requests.get(self.url, cookies=self.cookies)
         if r.status_code == 200:
-            print("Punch ok! (maybe :-D)")
+            print("Balance before: " + self.balance)
+            self.login()
+            print("Balance now: " + self.balance)
 
 
